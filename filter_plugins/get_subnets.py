b7cf3c0368e84f730d22d79abd80124b7b9d5b8d
Alexei Ledenev <alexei.ledenev@hp.com>
2015-08-05 17:25:54 +0300

    switching to ansible

---
+++ filter_plugins/get_subnets.py
@@ -0,0 +1,20 @@
+from jinja2.utils import soft_unicode
+
+def get_subnets(value, tag_key, tag_value, return_key='id'):
+    # return an attribute for all subnets that match
+    subnets = []
+    for item in value:
+      for key, value in item['resource_tags'].iteritems():
+        if key == tag_key and value == tag_value:
+          subnets.append(item[return_key])
+
+    return subnets
+
+
+class FilterModule(object):
+    ''' Ansible core jinja2 filters '''
+
+    def filters(self):
+        return {
+            'get_subnets': get_subnets,
+        }
