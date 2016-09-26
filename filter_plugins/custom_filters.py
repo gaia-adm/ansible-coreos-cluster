4a2e2007aba57af837e2e76ec86fcbd39975f6da
Alexei Ledenev <alexei.ledenev@hp.com>
2016-09-26 17:19:35 +0300

    refactoring towards idempotency

--- filter_plugins/custom_filters.py
+++ filter_plugins/custom_filters.py
@@ -19,19 +19,15 @@ def get_subnets(value, tag_key, tag_value, return_key='id'):
 
     return subnets
 
+def get_subnets_full(value, tag_key, tag_value):
+    # return subnets that match
+    subnets = []
+    for item in value:
+      for key, value in item['resource_tags'].iteritems():
+        if key == tag_key and value == tag_value:
+          subnets.append(item)
 
-def get_hosted_zone_id(value):
-    default_region=os.environ['AWS_DEFAULT_REGION']
-    region_value=default_region.replace('-','_')+"_"
-    print region_value
-    for zone in value:
-        print "Next zone: %s" % zone
-        if zone.startswith(region_value):
-            print "Selected: "+zone[len(region_value):]
-            return zone[len(region_value):]
-
-def has_volume_state(volume_info, state):
-    return len(volume_info['Volumes']) == 1 and volume_info['Volumes'][0]['State'] == state
+    return subnets
 
 # parses "aws rds describe-db-instances --db-instance-identifier <id>" output to fetch the entdpoint - must be used with db-instance-identifier
 def get_rds_endpoint(db_instance):
@@ -44,7 +40,6 @@ class FilterModule(object):
         return {
             'ec2_instance_info': ec2_instance_info,
             'get_subnets': get_subnets,
-            'get_hosted_zone_id': get_hosted_zone_id,
-            'has_volume_state': has_volume_state,
+            'get_subnets_full': get_subnets_full,
             'get_rds_endpoint': get_rds_endpoint
         }
